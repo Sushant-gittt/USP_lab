f3dc482ce8ba40ebe76f65339347dccd383f955b
Sushant <71494812+Sushant-gittt@users.noreply.github.com>
2021-12-16 10:31:09 +0530

    Update lab16.sh

--- lab16/lab16.sh
+++ lab16/lab16.sh
@@ -1,6 +1,9 @@
-echo "Enter file to open: "
-read f
-lines=`wc -l < $f`
-words=`wc -w < $f`
-characters=`wc -m < $f`
-echo "Lines = $lines \n Words = $words \n Characters = $characters"
+#!/bin/bash
+echo Enter the filename
+read file
+c=`cat $file | wc -c`
+w=`cat $file | wc -w`
+l=`grep -c "." $file`
+echo Number of characters in $file is $c
+echo Number of words in $file is $w
+echo Number of lines in $file is $l
